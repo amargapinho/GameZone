664461ef938f522b31b311446133f29acb983c6e
LichtBuch <91839302+LichtBuch@users.noreply.github.com>
2021-10-04 11:03:23 +0200

    added head.tpl.php

---
+++ src/tpl/head.tpl.php
@@ -0,0 +1,16 @@
+<head>
+    <script src="/src/js/bootstrap.bundle.min.js"></script>
+    <script src="/src/js/jquery-3.6.0.min.js"></script>
+    <script src="/src/js/jquery.dataTables.min.js"></script>
+    <script src="/src/js/dataTables.bootstrap5.min.js"></script>
+
+    <link href="/src/css/bootstrap.min.css">
+    <link href="/src/css/dataTables.bootstrap5.min.css">
+
+    <script>
+        $(document).ready( function () {
+            $('.datatable').DataTable();
+        });
+    </script>
+
+</head>
