d25f690c2ded55c1b304ff0df290c5d35e7cf618
LichtBuch <91839302+LichtBuch@users.noreply.github.com>
2021-10-07 11:42:12 +0200

    added first draft for Game Template

--- index.php
+++ index.php
@@ -3,6 +3,5 @@
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/src/tpl/head.tpl.php';
 
-?>
-
-Hello World
+include __DIR__ . '/src/tpl/game.tpl.php';
+include __DIR__ . '/src/tpl/gameModal.tpl.php';
--- src/tpl/game.tpl.php
+++ src/tpl/game.tpl.php
@@ -1,26 +1,49 @@
-<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+<div class="float-left p-1 sticky-top">
+  <button type="button" class="btn btn-primary menu-button"><i class="fas fa-arrow-left w-100 h-auto"></i></button>
+</div>
+<div class="float-right p-1 sticky-top">
+  <button type="button" class="btn btn-primary  menu-button"  data-toggle="modal" data-target="#gameModal"><i class="far fa-edit w-100 h-auto"></i></button>
+</div>
+
+<div class="container">
+
+  <h1 class="text-center py-5">Spiel Name</h1>
+
+  <div id="carouselExampleControls" class="carousel slide py-5" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item  text-center active">
+      <i class="fas fa-gamepad h-50 w-auto"></i>
+      </div>
+      <div class="carousel-item text-center">
+      <i class="fas fa-gamepad h-50 w-auto"></i>
+      </div>
+      <div class="carousel-item text-center">
+      <i class="fas fa-gamepad h-50 w-auto"></i>
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
   </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="..." class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
+
+  <h4 class="text-center py-5">
+    Preis (Amazon): 60€<br><br>
+    Bewertung <i class="fas fa-star"></i>
+  </h4>
+
+  <span class="text-center py-5">
+    Spiel Beschreibung:<br><br>
+    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+  </span>
+
 </div>
