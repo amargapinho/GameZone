17fcc55228da5cdf43393a16d2145b463f2621d8
LichtBuch <91839302+LichtBuch@users.noreply.github.com>
2021-10-07 11:02:02 +0200

    added fontawesome icons and custom.css

--- src/tpl/head.tpl.php
+++ src/tpl/head.tpl.php
@@ -3,9 +3,11 @@
     <script src="/src/js/bootstrap.bundle.min.js"></script>
     <script src="/src/js/jquery.dataTables.min.js"></script>
     <script src="/src/js/dataTables.bootstrap4.min.js"></script>
+    <script src="/src/js/all.min.js"></script>
 
     <link href="/src/css/bootstrap.min.css" rel="stylesheet">
     <link href="/src/css/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <link href="/src/css/custom.css" rel="stylesheet">
 
     <script>
         $(document).ready( function () {
