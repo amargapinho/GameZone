2ff5cc7dd573e679762bb94223d9eabe9006f221
LichtBuch <91839302+LichtBuch@users.noreply.github.com>
2021-10-07 10:21:53 +0200

    switched from bootstrap5 to bootstrap 4 for phpdesktop support

--- src/tpl/head.tpl.php
+++ src/tpl/head.tpl.php
@@ -1,11 +1,11 @@
 <head>
-    <script src="/src/js/bootstrap.bundle.min.js"></script>
     <script src="/src/js/jquery-3.6.0.min.js"></script>
+    <script src="/src/js/bootstrap.bundle.min.js"></script>
     <script src="/src/js/jquery.dataTables.min.js"></script>
-    <script src="/src/js/dataTables.bootstrap5.min.js"></script>
+    <script src="/src/js/dataTables.bootstrap4.min.js"></script>
 
-    <link href="/src/css/bootstrap.min.css">
-    <link href="/src/css/dataTables.bootstrap5.min.css">
+    <link href="/src/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/src/css/dataTables.bootstrap4.min.css" rel="stylesheet">
 
     <script>
         $(document).ready( function () {
