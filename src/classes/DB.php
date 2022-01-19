dc624a88ba66672736cf7b9dc467d1d3a79294f4
LichtBuch <91839302+LichtBuch@users.noreply.github.com>
2022-01-19 15:31:36 +0100

    database now regenerates
    When the Database is deleted the database.sql regenerates the database

--- src/classes/DB.php
+++ src/classes/DB.php
@@ -8,10 +8,18 @@ class DB extends PDO{
 
     use Singleton;
 
-    const DEFAULT_DB_PATH = __DIR__ . '/../db/database.db';
+	const DB_FOLDER = __DIR__ . '/../db/';
+	const SQL_PATH = self::DB_FOLDER . 'database.sql';
+    const DEFAULT_DB_PATH = self::DB_FOLDER . 'database.db';
 
     public function __construct($dbPath = self::DEFAULT_DB_PATH){
-        parent::__construct("sqlite:$dbPath");
+		if(file_exists($dbPath)){
+			parent::__construct("sqlite:$dbPath");
+		}else {
+			file_put_contents($dbPath, '');
+			parent::__construct("sqlite:$dbPath");
+			$this->exec(file_get_contents(self::SQL_PATH));
+		}
         $this->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     }
 
