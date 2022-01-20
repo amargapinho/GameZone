619b736b5d3893761b6c558a4f5e218b05250004
LichtBuch <91839302+LichtBuch@users.noreply.github.com>
2022-01-20 11:07:24 +0100

    CSVs are now closed and deleted after beeing imported

--- src/php/csvImport.inc.php
+++ src/php/csvImport.inc.php
@@ -9,8 +9,12 @@ if(isset($_GET['action']) && $_GET['action'] === 'importCSV'){
     move_uploaded_file($_FILES['GameFile']['tmp_name'], $filePath);
 
 	$file = fopen($filePath, 'r');
+
 	while (!feof($file)){
 		Game::importCSV(fgetcsv($file));
 	}
+
+	fclose($file);
+	unlink($filePath);
 
 }
