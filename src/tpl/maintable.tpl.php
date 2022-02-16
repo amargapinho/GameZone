c06b704520377bd868f4d5486b9e23476cd777fd
Aichurok00 <aichurowka791@gmail.com>
2022-02-16 17:42:42 +0100

    new Template

--- src/tpl/maintable.tpl.php
+++ src/tpl/maintable.tpl.php
@@ -4,100 +4,44 @@
 use GameZone\Game;
 
 ?>
-<table>
+<table class="dataTable">
     <thead>
+        <th>Name</th>
+        <th>Bescription</th>
+        <th>Images</th>
+        <th>Categorie</th>
+        <th>Review</th>
+        <th>Price</th>
+        <th>Wishlisted</th>
     </thead>
     <tbody>
         <?php foreach(Game::getAll() as $game):?>
             <tr>
-                <th>
-                    <?php print($game->getDescription()) ?>
-                </th>
+                <th><?= $game->getGameName()?></th>
+                <th><?= $game->getDescription() ?></th>
+                <th><?= $game->getImages()?></th>
+                <th><?= $game->getCategories()?></th>
+                <th><?= $game->getReleaseDate()?></th>
+                <th><?= $game->getPrice()?></th>
+                <th><?= $game->getReview()?></th>
+                <?php if($game->getReview()) :?>
+                    <th>
+                    <i class="fa-solid fa-star-sharp"></i>
+                    </th>
+                <?php endif; ?>
+                <?php if($game->isWishlisted()) :?>
+                        <th>
+                        <i class="fa-solid fa-circle-heart"></i>
+                        </th>
+                    <?php else: ?>
+                        <th>
+                        <i class="fa-solid fa-xmark"></i>
+                        </th>
+                    <?php endif; ?>
+                <th><?= $game->getPurchaseDate()?></th>
             </tr>
         <?php endforeach; ?>
-        <?php foreach(Game::getAll() as $gameName):?>
-            <tr>
-                <th>
-                    <?php print($gameName->getDescription()) ?>
-                </th>
-            </tr>
-        <?php endforeach; ?>
-        <?php foreach(Game::getAll() as $images):?>
-            <tr>
-                <th>
-                    <?php print( $images->getDescription()) ?>
-                </th>
-            </tr>
-        <?php endforeach; ?>
-        <?php foreach(Game::getAll() as $description):?>
-            <tr>
-                <th>
-                    <?php print($description->getDescription()) ?>
-                </th>
-            </tr>
-        <?php endforeach; ?>
-        <?php foreach(Game::getAll() as $categories):?>
-            <tr>
-                <th>
-                    <?php print($categories->getDescription()) ?>
-                </th>
-            </tr>
-        <?php endforeach; ?>
-        <?php foreach(Game::getAll() as $$releaseDate):?>
-            <tr>
-                <th>
-                    <?php print($releaseDate->getDescription()) ?>
-                </th>
-            </tr>
-        <?php endforeach; ?>
-        <?php foreach(Game::getAll() as $price):?>
-            <tr>
-                <th>
-                    <?php print($price->getDescription()) ?>
-                </th>
-            </tr>
-        <?php endforeach; ?>
-        <?php foreach(Game::getAll() as $review):?>
-            <tr>
-                <th>
-                    <?php print($review->getDescription()) ?>
-                </th>
-            </tr>
-        <?php endforeach; ?>
-        <?php foreach(Game::getAll() as $wishlisted):?>
-            <tr>
-                <th>
-                    <?php print($wishlisted->getDescription()) ?>
-                </th>
-            </tr>
-        <?php endforeach; ?>
-        <?php foreach(Game::getAll() as $favored):?>
-            <tr>
-                <th>
-                    <?php print($favored->getDescription()) ?>
-                </th>
-            </tr>
-        <?php endforeach; ?>
-        <?php foreach(Game::getAll() as $purchaseDate):?>
-            <tr>
-                <th>
-                    <?php print($purchaseDate->getDescription()) ?>
-                </th>
-            </tr>
-        <?php endforeach; ?>
-        <?php foreach(Game::getAll() as $deleted):?>
-            <tr>
-                <th>
-                    <?php print($deleted->getDescription()) ?>
-                </th>
-            </tr>
-        <?php endforeach; ?>
-
-
-        <?php
-        if(isset($_GET['$categories'] )&& isset($_GET['$gameName'] )&& isset($_GET['$favored']) &&isset($_GET['$review']))
-        {
-        }
-        ?>
+        
+       
     </tbody>
 </table>
